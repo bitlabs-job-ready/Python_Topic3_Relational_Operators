6cabc17ee69db5cdd6ecafb8d995cf2ddce776fd
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-03-21 12:33:20 +0530

    Add files via upload

---
+++ Python_Topic3_Relational_Operators.py
@@ -0,0 +1,37 @@
+# Relational Operators
+# In Place of Question Mark you need to write the condition along with Relational Operators
+
+a = 21
+b = 10
+
+if ( ? ):
+   print "Line 1 - a is equal to b"
+else:
+   print "Line 1 - a is not equal to b"
+
+if ( ? ):
+   print "Line 2 - a is not equal to b"
+else:
+   print "Line 2 - a is equal to b"
+
+if ( ? ):
+   print "Line 4 - a is less than b" 
+else:
+   print "Line 4 - a is not less than b"
+
+if ( ? ):
+   print "Line 5 - a is greater than b"
+else:
+   print "Line 5 - a is not greater than b"
+
+a = 5;
+b = 20;
+if ( ? ):
+   print "Line 6 - a is either less than or equal to  b"
+else:
+   print "Line 6 - a is neither less than nor equal to  b"
+
+if ( ? ):
+   print "Line 7 - b is either greater than  or equal to b"
+else:
+   print "Line 7 - b is neither greater than  nor equal to b"
